26fa910b77399cbb7937ae469d1ace34b55f8d87
ZTW21 <ztwilson2002@gmail.com>
2024-03-12 15:24:45 -0400

    authentication added, no styling on auth form yet

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "itis4340watertracker770e2817": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import WaterTracker from './components/WaterTracker';
+import './amplify-authenticator-custom.css';
+
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
-    <WaterTracker/>
+    <div className='amplify-authenticator-custom'>
+      <WaterTracker/>
+    </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
