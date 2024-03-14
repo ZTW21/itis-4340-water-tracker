c903a204d9b2c35bbee6f5da91f1086b476dd9a7
ZTW21 <ztwilson2002@gmail.com>
2024-03-14 12:34:22 -0400

    sign out button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import WaterTracker from './components/WaterTracker';
 import './amplify-authenticator-custom.css';
 
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import awsExports from './aws-exports';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 
 Amplify.configure(awsExports);
 
 function App() {
+  const signOut =  async () => {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log('error signing out: ', error);
+    }
+  
+  }
+
   return (
     <div className='amplify-authenticator-custom'>
+      <button 
+        onClick={signOut} 
+        style={{position: 'absolute', right: 20, top: 20}}
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Sign Out
+      </button>
       <WaterTracker/>
     </div>
   );
