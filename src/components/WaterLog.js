a8c608fbb88a79c6aaa2f4bd1b6bbb03295aed37
ZTW21 <ztwilson2002@gmail.com>
2024-03-18 15:41:49 -0400

    water log date/time formatting

--- src/components/WaterLog.js
+++ src/components/WaterLog.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-// displays a single water intake log
+// Displays a single water intake log
 function WaterLog({ log }) {
-  // renders the amount of water and the timestamp of intake
-  return <div>{`${log.amount} fl oz at ${log.timestamp}`}</div>;
+  // Convert the AWS timestamp to a Date object
+  const date = new Date(log.timestamp);
+
+  // Use toLocaleString for initial formatting
+  const options = {
+    month: 'numeric', day: 'numeric', year: '2-digit',
+    hour: '2-digit', minute: '2-digit', hour12: true
+  };
+  let formattedDate = date.toLocaleString('en-US', options);
+
+  // Remove leading zero from hours less than 10
+  formattedDate = formattedDate.replace(/(\s)0(\d:\d{2}\s\w{2})/, "$1$2");
+
+  // Renders the amount of water and the adjusted timestamp of intake
+  return <div>{`${log.amount} fl oz at ${formattedDate}`}</div>;
 }
 
 export default WaterLog;
